ea199844acb53cea5f1a58a2dce97ba54e91fd34
Sabrina Bueno <bina_fbueno@hotmail.com>
2024-07-03 20:29:08 -0300

    aplicando o blur

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,5 +1,9 @@
 const camposDoFormulario = document.querySelectorAll("[required]");
 
 camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => verificaCampo());
+    campo.addEventListener("blur", () => verificaCampo(campo));
 })
+
+function verificaCampo(campo) {
+    
+}
